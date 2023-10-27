6752035e25a25480f2be8d9901f16ac7eafa2a49
doodledood <aviram.kofman@gmail.com>
2023-10-27 19:35:47 +0300

    improve sturctured prompt

--- chat/structured_prompt.py
+++ chat/structured_prompt.py
@@ -1,30 +1,98 @@
-class StructuredPrompt(dict):
-    def __init__(self, parts: dict):
-        super().__init__(parts)
+import dataclasses
+from typing import Optional, List, Union
 
-    def format_dict(self, d: dict, level: int = 0) -> str:
-        parts = []
-        for k, v in d.items():
-            result = f'{"#" * (level + 1)} {k.upper() if level == 0 else k.capitalize()}\n'
 
-            if isinstance(v, dict):
-                result += self.format_dict(v, level + 1)
-            elif isinstance(v, list):
-                result += '\n'.join([f'- {item}' for item in v])
-            elif isinstance(v, tuple):
-                text, other = v
-                result += text + '\n\n' + self.format_dict(other, level + 1)
+@dataclasses.dataclass
+class Section:
+    name: str
+    content: Optional[Union[str, List[str]]] = None
+    sub_sections: Optional[List['Section']] = None
+
+    def to_text(self, level: int = 0) -> str:
+        result = f'{"#" * (level + 1)} {self.name.upper() if level == 0 else self.name.capitalize()}'
+
+        if self.content is not None:
+            if isinstance(self.content, list):
+                result += '\n'.join([f'- {item}' for item in self.content])
             else:
-                result += v
+                result += '\n' + self.content
 
-            parts.append(result)
-
-        result = '\n\n'.join(parts)
+        if self.sub_sections is not None:
+            for sub_section in self.sub_sections:
+                result += '\n\n' + sub_section.to_text(level + 1)
 
         return result
 
-    def __str__(self):
-        return self.format_dict(d=self)
+    @staticmethod
+    def from_dict(name: str, content: Union[str, List[str], dict]) -> 'Section':
+        if isinstance(content, dict):
+            sub_sections = []
 
-    def __repr__(self):
-        return str(self)
+            for sub_section_name, sub_section_content in content.items():
+                sub_sections.append(Section.from_dict(sub_section_name, sub_section_content))
+
+            return Section(name=name, sub_sections=sub_sections)
+        else:
+            return Section(name=name, content=content)
+
+
+@dataclasses.dataclass
+class StructuredPrompt:
+    sections: List[Section]
+
+    def to_text(self) -> str:
+        result = ''
+        for section in self.sections:
+            result += section.to_text() + '\n\n'
+
+        return result
+
+    @staticmethod
+    def from_dict(d: dict) -> 'StructuredPrompt':
+        sections = []
+
+        for name, contents in d.items():
+            sections.append(Section.from_dict(name, contents))
+
+        return StructuredPrompt(sections=sections)
+
+
+if __name__ == '__main__':
+    print(StructuredPrompt(
+        sections=[
+            Section(
+                name='Goal Identification',
+                content='Identify the goal of the decision you are trying to make. For example, if you are trying to decide which car to buy, your goal might be to buy the car that is the most reliable.',
+                sub_sections=[
+                    Section(
+                        name='Goal',
+                        content='Buy the car that is the most reliable.'
+                    ),
+                    Section(
+                        name='Alternatives',
+                        content=[
+                            'Buy a Honda Civic.',
+                            'Buy a Toyota Corolla.',
+                            'Buy a Ford Focus.'
+                        ]
+                    )
+                ]
+            ),
+            Section(
+                name='Criteria Identification',
+                content='Identify the criteria that you will use to evaluate the alternatives. For example, if you are trying to decide which car to buy, your criteria might be reliability and price.',
+                sub_sections=[
+                    Section(
+                        name='Criteria',
+                        content=[
+                            'Reliability',
+                            'Price'
+                        ],
+                        sub_sections=[
+                            Section(
+                                name='Reliability',
+                                content='The car that is the most reliable.'
+                            )
+                        ])
+                ]
+            )]).to_text())
