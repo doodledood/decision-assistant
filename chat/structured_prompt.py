b84d5df4a4968d1be4cfaa82b089ff0df5a53f48
doodledood <aviram.kofman@gmail.com>
2023-10-28 11:05:18 +0300

    improve structured prompt

--- chat/structured_prompt.py
+++ chat/structured_prompt.py
@@ -1,21 +1,22 @@
 import dataclasses
-from typing import Optional, List, Union
+from typing import Optional, List
 
 
 @dataclasses.dataclass
 class Section:
     name: str
-    content: Optional[Union[str, List[str]]] = None
+    text: Optional[str] = None
+    list: Optional[List[str]] = None
     sub_sections: Optional[List['Section']] = None
 
     def to_text(self, level: int = 0) -> str:
         result = f'{"#" * (level + 1)} {self.name.upper() if level == 0 else self.name.capitalize()}'
 
-        if self.content is not None:
-            if isinstance(self.content, list):
-                result += '\n'.join([f'- {item}' for item in self.content])
-            else:
-                result += '\n' + self.content
+        if self.text is not None:
+            result += '\n' + self.text
+
+        if self.list is not None:
+            result += '\n' + '\n'.join([f'- {item}' for item in self.list])
 
         if self.sub_sections is not None:
             for sub_section in self.sub_sections:
@@ -23,38 +24,39 @@ class Section:
 
         return result
 
-    @staticmethod
-    def from_dict(name: str, content: Union[str, List[str], dict]) -> 'Section':
-        if isinstance(content, dict):
-            sub_sections = []
-
-            for sub_section_name, sub_section_content in content.items():
-                sub_sections.append(Section.from_dict(sub_section_name, sub_section_content))
-
-            return Section(name=name, sub_sections=sub_sections)
-        else:
-            return Section(name=name, content=content)
-
 
 @dataclasses.dataclass
 class StructuredPrompt:
     sections: List[Section]
 
-    def to_text(self) -> str:
+    def __getitem__(self, item):
+        assert isinstance(item, str)
+
+        relevant_sections = [section for section in self.sections if section.name == item]
+        if len(relevant_sections) == 0:
+            raise KeyError(f'No section with name {item} exists.')
+
+        return relevant_sections[0]
+
+    def __setitem__(self, key, value):
+        assert isinstance(key, str)
+        assert isinstance(value, Section)
+
+        try:
+            section = self[key]
+            section.text = value
+        except KeyError:
+            self.sections.append(value)
+
+    def __str__(self) -> str:
         result = ''
         for section in self.sections:
             result += section.to_text() + '\n\n'
 
         return result
 
-    @staticmethod
-    def from_dict(d: dict) -> 'StructuredPrompt':
-        sections = []
-
-        for name, contents in d.items():
-            sections.append(Section.from_dict(name, contents))
-
-        return StructuredPrompt(sections=sections)
+    def __repr__(self):
+        return self.__str__()
 
 
 if __name__ == '__main__':
@@ -62,15 +64,16 @@ if __name__ == '__main__':
         sections=[
             Section(
                 name='Goal Identification',
-                content='Identify the goal of the decision you are trying to make. For example, if you are trying to decide which car to buy, your goal might be to buy the car that is the most reliable.',
+                text='Identify the goal of the decision you are trying to make. For example, if you are trying to decide which car to buy, your goal might be to buy the car that is the most reliable.',
                 sub_sections=[
                     Section(
                         name='Goal',
-                        content='Buy the car that is the most reliable.'
+                        text='Buy the car that is the most reliable.'
                     ),
                     Section(
                         name='Alternatives',
-                        content=[
+                        text='Here are some alternatives to consider:',
+                        list=[
                             'Buy a Honda Civic.',
                             'Buy a Toyota Corolla.',
                             'Buy a Ford Focus.'
@@ -80,19 +83,19 @@ if __name__ == '__main__':
             ),
             Section(
                 name='Criteria Identification',
-                content='Identify the criteria that you will use to evaluate the alternatives. For example, if you are trying to decide which car to buy, your criteria might be reliability and price.',
+                text='Identify the criteria that you will use to evaluate the alternatives. For example, if you are trying to decide which car to buy, your criteria might be reliability and price.',
                 sub_sections=[
                     Section(
                         name='Criteria',
-                        content=[
+                        list=[
                             'Reliability',
                             'Price'
                         ],
                         sub_sections=[
                             Section(
                                 name='Reliability',
-                                content='The car that is the most reliable.'
+                                text='The car that is the most reliable.'
                             )
                         ])
                 ]
-            )]).to_text())
+            )]))
