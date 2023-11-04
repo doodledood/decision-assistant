61da6b720b0b1e3c6acc785fee89bea99fc4a31b
doodledood <aviram.kofman@gmail.com>
2023-11-04 11:21:38 +0200

    scraper api retriver is actually a simple request page retriver as well

--- chat/web_research/page_retriever.py
+++ chat/web_research/page_retriever.py
@@ -13,37 +13,6 @@ class PageRetriever(abc.ABC):
         raise NotImplementedError()
 
 
-class ScraperAPIPageRetriever(PageRetriever):
-    def __init__(self, api_key: Optional[str] = None, render_js: bool = False):
-        if api_key is None:
-            if 'SCRAPERAPI_API_KEY' not in os.environ:
-                raise ValueError('SCRAPERAPI_API_KEY environment variable is required or api_key argument.')
-
-            api_key = os.environ['SCRAPERAPI_API_KEY']
-
-        self.api_key = api_key
-        self.render_js = render_js
-
-    @retry(retry=retry_if_exception_type(TransientHTTPError),
-           wait=wait_fixed(2) + wait_random(0, 2),
-           stop=stop_after_attempt(5))
-    def retrieve_html(self, url: str) -> str:
-        payload = {
-            'api_key': self.api_key,
-            'url': url,
-            'render': self.render_js
-        }
-        r = requests.get('https://api.scraperapi.com/', params=payload)
-
-        if r.status_code < 300:
-            return r.text
-
-        if r.status_code >= 500:
-            raise TransientHTTPError(r.status_code, r.text)
-
-        raise NonTransientHTTPError(r.status_code, r.text)
-
-
 class SimpleRequestsPageRetriever(PageRetriever):
     def __init__(self, **kwargs):
         self.kwargs = kwargs
@@ -60,3 +29,24 @@ class SimpleRequestsPageRetriever(PageRetriever):
             raise TransientHTTPError(r.status_code, r.text)
 
         raise NonTransientHTTPError(r.status_code, r.text)
+
+
+class ScraperAPIPageRetriever(SimpleRequestsPageRetriever):
+    def __init__(self, api_key: Optional[str] = None, render_js: bool = False):
+        super().__init__()
+
+        if api_key is None:
+            if 'SCRAPERAPI_API_KEY' not in os.environ:
+                raise ValueError('SCRAPERAPI_API_KEY environment variable is required or api_key argument.')
+
+            api_key = os.environ['SCRAPERAPI_API_KEY']
+
+        self.api_key = api_key
+        self.render_js = render_js
+
+    def retrieve_html(self, url: str) -> str:
+        return super().retrieve_html(url, params={
+            'api_key': self.api_key,
+            'url': url,
+            'render': self.render_js
+        })
